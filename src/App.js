bb85f6d6ec934cbe9923d9330a614ab72cf5b5db
luvthatbih <luvthatbih@gmail.com>
2024-08-16 20:01:34 +0300

    initial

--- src/App.js
+++ src/App.js
@@ -1,22 +1,12 @@
 
-import { useEffect } from 'react';
 import './App.css';
 
-const tg = window.Telegram.WebApp;
 
 function App() {
 
-  useEffect(() => {
-    tg.ready();
-  })
-
-  const onConsole = () => {
-    tg.close()
-  }
 
   return (
     <div className="App">
-        <button onClick={onConsole}>Сейчас все закроется</button>
     </div>
   );
 }
