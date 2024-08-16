dc5245d2583d8f96f7fa17f28780daca4f61353d
luvthatbih <luvthatbih@gmail.com>
2024-08-16 20:19:23 +0300

    initial

--- src/App.js
+++ src/App.js
@@ -1,12 +1,21 @@
 
+import { useEffect } from 'react';
 import './App.css';
-
+import { useTelegram } from './hooks/useTelegram';
 
 function App() {
 
+  const {onToggleButton, tg} = useTelegram();
+
+  useEffect(() => {
+    tg.ready();
+  }
+
+  )
 
   return (
     <div className="App">
+      <button onClick={onToggleButton}>toggle</button>
     </div>
   );
 }
